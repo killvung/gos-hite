a02a892ce5664892b4277d9e998051642fa26cbf
killvung <killvung@gmail.com>
2021-04-16 16:07:33 -0500

    Initial commit

---
+++ dotenv-example/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func loadFromDotEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		// By the way, you don't "throw", you "log"
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func getEnv(key string) string {
+	return os.Getenv(key)
+}
+
+func main() {
+	fmt.Println("Hello world")
+	value := os.Getenv("env")
+	if value == "" || value == "dev" {
+		loadFromDotEnv()
+	}
+	println(getEnv("ENV"))
+}
